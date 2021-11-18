beb8afb493f6a5f541c383a524785d5155b4ae73
Antony Maia <antonymaiati@gmail.com>
2021-11-18 19:44:22 -0300

    first commit

---
+++ src/main/java/br/antony/sea/SeaApplication.java
@@ -0,0 +1,13 @@
+package br.antony.sea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaApplication.class, args);
+	}
+
+}
